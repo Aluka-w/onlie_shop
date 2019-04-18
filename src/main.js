d1524dff0d55f83d560fc95246f697b907ab6c6f
王斌 <wangbin1441@163.com>
2019-04-18 21:54:05 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 // import 'lib-flexible';
 import "@a/css/reset.css";
 import "@a/css/mediaQuery.css";
 
 Vue.config.productionTip = false;
+// 把axios挂载到全局
+Vue.prototype.$axios = axios;
 
 new Vue({
   router,
